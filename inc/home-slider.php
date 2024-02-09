3a6c95eb857393e4111f6752aae4f8760927b8df
oleg.u <oleg.u@thewhitelabelagency.com>
2024-02-09 13:01:04 +0200

    Home slider button.

--- inc/home-slider.php
+++ inc/home-slider.php
@@ -1,7 +1,7 @@
 <?php
 // Register Post Type Slider
-add_action( 'init', function () {
-    \theme\Util::registerPostType( 'slider', 'Slide', 'Slides', [
+add_action('init', function () {
+    \theme\Util::registerPostType('slider', 'Slide', 'Slides', [
         'public'        => false,
         'hierarchical'  => true,
         'menu_icon'     => 'dashicons-format-gallery',
@@ -13,18 +13,18 @@ add_action( 'init', function () {
             'editor',
             'post-formats'
         ],
-    ] );
-    add_theme_support( 'post-formats', [ 'video' ] );
-    remove_post_type_support( 'post', 'post-formats' );
-} );
+    ]);
+    add_theme_support('post-formats', [ 'video' ]);
+    remove_post_type_support('post', 'post-formats');
+});
 
-add_action( 'add_meta_boxes', function () {
+add_action('add_meta_boxes', function () {
     $screens = [ 'slider' ];
     add_meta_box(
         'slide_background',
-        __( 'Slide background', 'fxy' ),
-        function ( $post, $meta ) {
-            wp_nonce_field( 'save_video_bg', 'project_nonce' ); ?>
+        __('Slide background', 'fxy'),
+        function ($post, $meta) {
+            wp_nonce_field('save_video_bg', 'project_nonce'); ?>
             <style>
                 .fields-list {
                     margin-left: -12px;
@@ -48,30 +48,30 @@ add_action( 'add_meta_boxes', function () {
                 <div class="field-wrap" style="width: 70%">
                     <p class="label-wrapper">
                         <label for="slide_video" style="display: block;">
-                            <b><?php _e( 'Video background', 'fxy' ); ?></b>
+                            <b><?php _e('Video background', 'fxy'); ?></b>
                         </label>
-                        <em><?php _e( 'Enter here link to video from Media Library or YouTube', 'fxy' ); ?></em>
+                        <em><?php _e('Enter here link to video from Media Library or YouTube', 'fxy'); ?></em>
                     </p>
                     <input
                         type="text"
                         id="slide_video"
                         name="slide_video_bg"
-                        value="<?php echo get_post_meta( $post->ID, 'slide_video_bg', true ); ?>"
+                        value="<?php echo get_post_meta($post->ID, 'slide_video_bg', true); ?>"
                         style="width: 100%;"
                     />
                 </div>
                 <div class="field-wrap" style="width: 30%">
                     <p class="label-wrapper">
                         <label for="video_aspect_ratio" style="display: block;">
-                            <b><?php _e( 'Video aspect ratio', 'fxy' ); ?></b>
+                            <b><?php _e('Video aspect ratio', 'fxy'); ?></b>
                         </label>
                     </p>
-                    <?php $aspect_ratio = get_post_meta( $post->ID, 'video_aspect_ratio', true ) ?: '16:9'; ?>
+                    <?php $aspect_ratio = get_post_meta($post->ID, 'video_aspect_ratio', true) ?: '16:9'; ?>
                     <?php $ratio_list = [ '16:9', '4:3', '2.39:1' ]; ?>
                     <select name="video_aspect_ratio" id="video_aspect_ratio" style="width: 100%;">
-                        <?php foreach ( $ratio_list as $item ) : ?>
+                        <?php foreach ($ratio_list as $item) : ?>
                             <option
-                                value="<?php echo $item; ?>" <?php selected( $aspect_ratio, $item ); ?>><?php echo $item; ?></option>
+                                value="<?php echo $item; ?>" <?php selected($aspect_ratio, $item); ?>><?php echo $item; ?></option>
                         <?php endforeach; ?>
                     </select>
                 </div>
@@ -81,34 +81,34 @@ add_action( 'add_meta_boxes', function () {
         },
         $screens
     );
-} );
+});
 
 /**
  * Update slide background on slide save
  */
-add_action( 'save_post', function ( $post_id ) {
-    if ( ! isset( $_POST['slide_video_bg'] ) && ! isset( $_POST['video_aspect_ratio'] ) ) {
+add_action('save_post', function ($post_id) {
+    if (! isset($_POST['slide_video_bg']) && ! isset($_POST['video_aspect_ratio'])) {
         return;
     }
-    if ( ! wp_verify_nonce( $_POST['project_nonce'], 'save_video_bg' ) ) {
+    if (! wp_verify_nonce($_POST['project_nonce'], 'save_video_bg')) {
         return;
     }
-    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
         return;
     }
-    if ( ! current_user_can( 'edit_post', $post_id ) ) {
+    if (! current_user_can('edit_post', $post_id)) {
         return;
     }
 
-    update_post_meta( $post_id, 'video_aspect_ratio', $_POST['video_aspect_ratio'] );
-    update_post_meta( $post_id, 'slide_video_bg', $_POST['slide_video_bg'] );
-} );
+    update_post_meta($post_id, 'video_aspect_ratio', $_POST['video_aspect_ratio']);
+    update_post_meta($post_id, 'slide_video_bg', $_POST['slide_video_bg']);
+});
 
 /**
  * Print script to hande appearance of metabox
  */
 $fxy_display_metaboxes = function () {
-    if ( get_post_type() == 'slider' ) {
+    if (get_post_type() == 'slider') {
         ?>
         <script type="text/javascript">// <![CDATA[
             $ = jQuery;
@@ -133,10 +133,10 @@ $fxy_display_metaboxes = function () {
 };
 
 //add_action('admin_enqueue_scripts', $fxy_display_metaboxes);
-add_action( 'admin_footer', $fxy_display_metaboxes );
+add_action('admin_footer', $fxy_display_metaboxes);
 
 // HOME Slider Shortcode
-add_shortcode( 'slider', function () {
+add_shortcode('slider', function () {
     ob_start();
     ?>
     <script type="text/javascript">
@@ -220,22 +220,22 @@ add_shortcode( 'slider', function () {
     </script>
 
     <?php
-    $slider = new WP_Query( [
+    $slider = new WP_Query([
         'post_type'      => 'slider',
         'order'          => 'ASC',
         'orderby'        => 'menu_order',
         'posts_per_page' => - 1
-    ] );
-    if ( $slider->have_posts() ) : ?>
+    ]);
+    if ($slider->have_posts()) : ?>
         <div id="home-slider" class="slick-slider home-slider">
-            <?php while ( $slider->have_posts() ) :
+            <?php while ($slider->have_posts()) :
                 $slider->the_post(); ?>
                 <div class="slick-slide home-slide">
-                    <div class="home-slide__inner" <?php bg( get_attached_img_url( get_the_ID(), 'full_hd' ) ); ?>>
-                        <?php $bg_video_url = get_post_meta( get_the_ID(), 'slide_video_bg', true ); ?>
-                        <?php if ( get_post_format() == 'video' && $bg_video_url ) : ?>
+                    <div class="home-slide__inner" <?php bg(get_attached_img_url(get_the_ID(), 'full_hd')); ?>>
+                        <?php $bg_video_url = get_post_meta(get_the_ID(), 'slide_video_bg', true); ?>
+                        <?php if (get_post_format() == 'video' && $bg_video_url) : ?>
                             <div class="videoHolder"
-                                 data-ratio="<?php echo get_post_meta( get_the_ID(), 'video_aspect_ratio', true ) ?: '16:9'; ?>">
+                                 data-ratio="<?php echo get_post_meta(get_the_ID(), 'video_aspect_ratio', true) ?: '16:9'; ?>">
                                 <?php
                                 $allowed_video_format = [
                                     'webm' => 'video/webm',
@@ -243,20 +243,20 @@ add_shortcode( 'slider', function () {
                                     'ogv'  => 'video/ogg',
                                     'mkv'  => 'video/mkv',
                                 ]; ?>
-                                <?php $file_info = wp_check_filetype( $bg_video_url, $allowed_video_format ); ?>
-                                <?php if ( $file_info['ext'] ) : ?>
+                                <?php $file_info = wp_check_filetype($bg_video_url, $allowed_video_format); ?>
+                                <?php if ($file_info['ext']) : ?>
                                     <video src="<?php echo $bg_video_url; ?>"
                                            autoplay
                                            preload="none"
                                            muted="muted"
                                            loop="loop"
                                            class="video video--local"></video>
-                                <?php elseif ( is_embed_video( $bg_video_url ) ) : ?>
+                                <?php elseif (is_embed_video($bg_video_url)) : ?>
                                     <div class="video video--embed responsive-embed widescreen">
-                                        <?php echo wp_oembed_get( $bg_video_url, [
+                                        <?php echo wp_oembed_get($bg_video_url, [
                                             'location' => 'home_slider',
                                             'id'       => 'slide-' . get_the_ID()
-                                        ] ); ?>
+                                        ]); ?>
                                     </div>
                                 <?php endif; ?>
                             </div>
@@ -271,9 +271,11 @@ add_shortcode( 'slider', function () {
                                             <article>
                                                 <?php the_content(); ?>
                                             </article>
-                                            <a href="" class="button">
-                                                <?php _e( 'JOIN OUR TEAM' ); ?>
-                                            </a>
+                                            <?php if ($slide_button = get_field('slide_button')) : ?>
+                                                <a href="<?php echo $slide_button['url']; ?>" class="button">
+                                                    <?php echo $slide_button['title']; ?>
+                                                </a>
+                                            <?php endif; ?>
                                         </div>
                                     </div>
                                 </div>
@@ -288,4 +290,4 @@ add_shortcode( 'slider', function () {
     wp_reset_query();
 
     return ob_get_clean();
-} );
+});
